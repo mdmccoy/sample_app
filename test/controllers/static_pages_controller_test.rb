c862460344d8d3ca5f208298d7cefecd86c0f596
mdmccoy <mdmccoy@gmail.com>
2018-01-31 17:05:32 -0500

    Added custom helper.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,10 +5,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test 'should get home' do
+  test 'should get base title' do
     get root_url
     assert_response :success
-    assert_select 'title', "Home | #{@base_title}"
+    assert_select 'title', @base_title.to_s
   end
 
   test 'should get help' do
