4ada8aff19dc457cf4d9611a37dec4aa61bcb9dc
mdmccoy <mdmccoy@gmail.com>
2018-02-08 14:46:56 -0500

    Mock up actions for logging in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,16 @@
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      # do something about logging in
+    else
+      flash('Incorrect email/password.')
+      render 'new'
+    end
+  end
+
+  def destroy; end
 end
