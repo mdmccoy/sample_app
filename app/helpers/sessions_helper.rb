9fb9910f4b784b46080287af98f60eadcc80399c
mdmccoy <mdmccoy@gmail.com>
2018-02-09 11:20:16 -0500

    Current user helper method.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,12 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  def current_user
+    if @current_user.nil?
+      @current_user = User.find_by(id: settings[:user_id])
+    else
+      @current_user
+    end
+  end
 end
